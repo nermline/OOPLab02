c8d447adcc1c61cc5ac4d9cabd7afa97cf51ca18
Mykola Titov <kolatitov132@gmail.com>
2025-11-29 22:02:49 +0200

    Update Filter method

--- DormDataService.cs
+++ DormDataService.cs
@@ -7,7 +7,7 @@ namespace Lab02
 {
     internal class DormDataService
     {
-        private IParsingStrategy _parsingStrategy;
+        private IParsingStrategy? _parsingStrategy;
         private List<Student> _allStudents;
 
         public DormDataService()
@@ -23,39 +23,67 @@ namespace Lab02
         public List<Student> LoadData(string filePath)
         {
             if (_parsingStrategy == null)
-                throw new System.Exception("Стратегія аналізу не обрана.");
+                throw new Exception("Стратегія аналізу не обрана.");
 
             _allStudents = _parsingStrategy.Parse(filePath);
             return _allStudents;
         }
 
-        public List<Student> Filter(string lastName, string firstName, string patronymic, string faculty, string course, string city, bool onlyScholarship)
+        public List<Student> Filter(Student filter)
         {
+
             var query = _allStudents.AsEnumerable();
 
-            if (!string.IsNullOrWhiteSpace(lastName))
-                query = query.Where(s => !string.IsNullOrEmpty(s.LastName) && s.LastName.Contains(lastName, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(filter.LastName))
+                query = query.Where(s => !string.IsNullOrEmpty(s.LastName)
+                                         && s.LastName.Contains(filter.LastName, StringComparison.OrdinalIgnoreCase));
 
-            if (!string.IsNullOrWhiteSpace(firstName))
-                query = query.Where(s => !string.IsNullOrEmpty(s.FirstName) && s.FirstName.Contains(firstName, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(filter.FirstName))
+                query = query.Where(s => !string.IsNullOrEmpty(s.FirstName)
+                                         && s.FirstName.Contains(filter.FirstName, StringComparison.OrdinalIgnoreCase));
 
-            if (!string.IsNullOrWhiteSpace(patronymic))
-                query = query.Where(s => !string.IsNullOrEmpty(s.Patronymic) && s.Patronymic.Contains(patronymic, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(filter.Patronymic))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Patronymic)
+                                         && s.Patronymic.Contains(filter.Patronymic, StringComparison.OrdinalIgnoreCase));
 
-            if (!string.IsNullOrWhiteSpace(faculty))
-                query = query.Where(s => !string.IsNullOrEmpty(s.Faculty) && s.Faculty.Contains(faculty, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(filter.Faculty))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Faculty)
+                                         && s.Faculty.Contains(filter.Faculty, StringComparison.OrdinalIgnoreCase));
 
-            if (!string.IsNullOrWhiteSpace(course))
-                query = query.Where(s => s.Course == course);
+            if (!string.IsNullOrWhiteSpace(filter.Specialty))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Specialty)
+                                         && s.Specialty.Contains(filter.Specialty, StringComparison.OrdinalIgnoreCase));
 
-            if (!string.IsNullOrWhiteSpace(city))
-                query = query.Where(s => !string.IsNullOrEmpty(s.City) && s.City.Contains(city, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrWhiteSpace(filter.Chair))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Chair)
+                                         && s.Chair.Contains(filter.Chair, StringComparison.OrdinalIgnoreCase));
 
-            if (onlyScholarship)
+            query = query.Where(s => s.Course == filter.Course);
+
+            if (!string.IsNullOrWhiteSpace(filter.Address))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Address)
+                                         && s.Address.Contains(filter.Address, StringComparison.OrdinalIgnoreCase));
+
+            if (!string.IsNullOrWhiteSpace(filter.Room))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Room)
+                                         && s.Room.Contains(filter.Room, StringComparison.OrdinalIgnoreCase));
+
+            if (!string.IsNullOrWhiteSpace(filter.Status))
+                query = query.Where(s => !string.IsNullOrEmpty(s.Status)
+                                         && s.Status.Equals(filter.Status, StringComparison.OrdinalIgnoreCase));
+
+            if (filter.FromDate != null)
+                query = query.Where(s => s.FromDate != null && s.FromDate >= filter.FromDate);
+
+            if (filter.ToDate != null)
+                query = query.Where(s => s.ToDate != null && s.ToDate <= filter.ToDate);
+
+            if (filter.HasScholarship)
                 query = query.Where(s => s.HasScholarship);
 
             return query.ToList();
         }
+
 
         public void ClearData()
         {
